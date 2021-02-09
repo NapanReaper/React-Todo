a75799ffde82a98ab5cd2f7c3b600ff7af056829
Do Tran Nhat Duy <napanreaper2506@gmail.com>
2021-02-09 14:01:57 +0700

    add Read Delete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
-const Todo = () => {
+const Todo = ({ todo, handleComplete, handleDelete }) => {
+
  return (
-  <div className='todo'>
-   <li className="todo-item">
-    Test
-   </li>
-   <button className="complete-btn">
+  <div className='todo' >
+   <li className={`todo-item ${todo.completed ? 'completed' : ''}`} >{todo.text}</li>
+   <button className="complete-btn" onClick={() => handleComplete(todo.id)}>
     <i className="fas fa-check"></i>
    </button>
-   <button className='trash-btn'>
-    <i i className="fas fa-trash"></i>
+   <button className='trash-btn' onClick={() => handleDelete(todo.id)}>
+    <i className="fas fa-trash"></i>
    </button>
   </div>
  )
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,23 @@
 import Todo from "./Todo"
 
 const TodoList = ({ todos, setTodos }) => {
+ const handleDelete = (id) => {
+  setTodos(todos.filter(todo => todo.id !== id))
+ }
+ const handleComplete = (id) => {
+  setTodos(todos.map(todo => {
+   if (todo.id === id) {
+    return { ...todo, completed: !todo.completed }
+   }
+   return todo
+  }))
+ }
  return (
   <div className='todo-container'>
    <ul className="todo-list">
-    <Todo />
+    {todos.map(todo => (
+     <Todo todo={todo} key={todo.id} handleComplete={handleComplete} handleDelete={handleDelete} />
+    ))}
    </ul>
   </div>
  )
